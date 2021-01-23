93a7993662739df41adf50e057618eacecd9f056
RUTURAJPC\rutur <jadhavrj@mail.uc.edu>
2021-01-22 21:00:03 -0500

    Setup Routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import checkoutSummary from './components/Order/CheckoutSummary/CheckoutSummary';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Layout>
-          <BurgerBuilder />
-          <Checkout />
-        </Layout>
-      </div>
+        <div>
+          <Layout>
+            <Switch>
+              <Route path="/checkout" component={Checkout}/>
+              <Route path="/" exact component={BurgerBuilder}/>
+            </Switch>
+          </Layout>
+        </div>
     );
   }
 }
